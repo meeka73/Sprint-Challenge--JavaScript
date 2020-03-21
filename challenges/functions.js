718d9455711f370dd44d00c59826f7ae21bb8747
Damika Weems <sweetcaramel0619@gmail.com>
2020-03-21 13:18:16 -0400

    Closures Challenge Complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -41,7 +41,7 @@ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice t
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: Because it is nested inside the function 'myFunction' scope.
 
 
 const external = "I'm outside the function";
