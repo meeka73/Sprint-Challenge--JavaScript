80e6b8775d2d33c5302dde07728128c1b85024c8
Damika Weems <sweetcaramel0619@gmail.com>
2020-03-21 13:43:03 -0400

    Classes Challenge Complete

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,7 +1,73 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+function CuboidMaker(options) {
+    this.length = options.length;
+    this.width = options.width;
+    this.height = options.height;
+  }
+  
+  CuboidMaker.prototype.volume = function() {
+    return this.length * this.width * this.height;
+  }
+  
+  CuboidMaker.prototype.surfaceArea = function () {
+    return 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height));
+  }
+  
+  const cuboid = new CuboidMaker({
+    length: 4,
+    width: 5,
+    height: 5,
+  })
+
+
+class CuboidMaker {
+    constructor(options) {
+      this.length = options.length;
+      this.width = options.width;
+      this.height = options.height;
+    }
+}
+
+volume() {
+  return this.length * this.width * this.height;
+}
+
+surfaceArea() {
+  return 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height));
+}
+
+class Cuboid extends CuboidMaker {
+  constructor(attributes) {
+    super(attributes);
+  }
+
+  volume() {
+    return Math.pow(this.length, 3);
+  }
+
+  surfaceArea() {
+    return 6 * Math.pow(this.length, 2);
+  }
+
+  surfaceArea() {
+    return 6 * Math.pow(this.length, 2);
+  }
+}
+
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5,
+})
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+class CubeMaker extends CuboidMaker {
+    constructor(volume, surfaceArea) {
+        
+    }
+}
